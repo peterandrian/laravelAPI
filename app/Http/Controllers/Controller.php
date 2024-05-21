b930714a4c0daccd397e09ff79a23c0f4c09cb0f
peter <peterandrianpriestley@gmail.com>
2024-05-22 02:14:27 +0700

    final

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,7 +1,24 @@
 <?php
 
 namespace App\Http\Controllers;
+use OpenApi\Annotations as OA;
 
+/**
+ * @OA\Info(
+ *      version="1.0",
+ *      title="My Rest API",
+ *      description="Laravel project to practice developing Rest API with L5 Swagger OpenAPI",
+ *      x={
+ *          "logo": {
+ *              "url": "https://miro.medium.com/v2/resize:fit:1200/1*J3G3akaMpUOLegw0p0qthA.png"
+ *          }
+ *      },
+ *      @OA\Contact(
+ *          name="Peter Andrian Priestley",
+ *          email="peter.422023020@civitas.ukrida.ac.id"
+ *      ),
+ * )
+ */
 abstract class Controller
 {
     //
--- app/Models/Book.php
+++ app/Models/Book.php
@@ -6,6 +6,24 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Models\User;
+use OpenApi\Annotations as OA;
+
+
+/**
+ * Class Book.
+ * 
+ * @author  Peter <peter.422023020@civitas.ukrida.ac.id>
+ * 
+ * @OA\Schema(
+ *     description="Book model",
+ *     title="Book model",
+ *     required={"title", "author"},
+ *     @OA\Xml(
+ *         name="Book"
+ *     )
+ * )
+ */
+
 
 class Book extends Model
 {
